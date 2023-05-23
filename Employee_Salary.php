495f0f4c7dba427e17218d8d183c0e077a19df5a
tatchooo <127770466+tatchooo@users.noreply.github.com>
2023-05-23 13:20:07 +0800

    Add files via upload

---
+++ Employee_Salary.php
@@ -0,0 +1,20 @@
+
+<?php
+
+require "config.php";
+
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    try {
+
+        $emp_no = $_POST['emp_no'];
+        $_SESSION['emp_no'] = $emp_no;
+        header('Location: Salary_Table.php');
+
+
+    } catch (PDOException $e) {
+        error_log($e->getMessage());
+        echo "<h1 style='color: red'>" . $e->getMessage() . "</h1>";
+    }
+}
